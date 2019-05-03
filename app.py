d911918bb430be133ff5f68f8e1b6d85abe0ec85
Fabien Lenoir <flenoir@gmail.com>
2019-05-03 16:13:45 +0200

    added google maps search and refactored in OOP

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
 #! /usr/bin/env python
 # coding: utf-8
 
-from classes import Question
-
+from classes import Question, GoogleMapsSearch
 
 inputQuestion = input("Please enter your question : ")
 
+# instanciation of question
 new_question = Question(inputQuestion)
 print(new_question.tokenize)
+
+
+# instanciation of Google maps search from question
+new_search = GoogleMapsSearch(new_question.tokenize)
+search_result = new_search.make_search()
+
+print(search_result['results'][0]['formatted_address'])
--- classes.py
+++ classes.py
@@ -1,5 +1,6 @@
 import nltk
 from nltk.corpus import stopwords
+import googlemaps
 
 en_stopwords = set(stopwords.words('english'))
 
@@ -18,4 +19,18 @@ class Question:
                 array.append(word)
         
         return ' '.join(array)
+
+
+class GoogleMapsSearch:
+
+    KEY = 'AIzaSyCnu18GjJrqGyvQ3DMECincwFAslFeGTu4'
+
+    def __init__(self, search):
+        self.search = search
+      
+    
+    def make_search(self):
+        gmaps = googlemaps.Client(key=GoogleMapsSearch.KEY)
+        result = gmaps.places(str(self.search))
+        return result
 
