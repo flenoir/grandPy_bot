950329d5853ac48a0468aa65320e510de47df382
flenoir <flenoir@gmail.com>
2019-05-11 10:57:45 +0200

    added boostrap theme and started to interact with flask

--- gpbapp/views.py
+++ gpbapp/views.py
@@ -7,7 +7,7 @@ app.config.from_object('config')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', data="A bot to answer all your questions")
 
 if __name__ == "__main__":
     app.run()
