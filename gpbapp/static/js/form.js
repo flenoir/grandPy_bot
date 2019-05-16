b2c479cf1f6b35a855bb7278c29221f6f2add9e4
Fabien Lenoir <flenoir@gmail.com>
2019-05-16 17:06:07 +0200

    added spinner

--- gpbapp/static/js/form.js
+++ gpbapp/static/js/form.js
@@ -1,23 +1,28 @@
 $(document).ready(function(){
     
     $('form').on('submit', function(event) {
-
+        
         $.ajax({
             url : '/process',
             data : {
                 // the key will be used as value in "request.form" in views.py
                 answer : $('#inlineFormInput').val(),
             },
-            type : 'POST'
+            type : 'POST',
+            beforeSend: function(){
+                $('#spinner').show();
+            },
+            success: function(data){
+                $('#spinner').hide()
+                if (data.error){
+                    $('#answer').text(data.error).show()
+                }
+                else {
+                    $('#answer').text(data.answer).show()
+                }
+            }
         })
-        .done(function(data) {
-            if (data.error){
-                $('#answer').text(data.error).show()
-            }
-            else {
-                $('#answer').text(data.answer).show()
-            }
-        });
+        
     event.preventDefault();
     });
 
